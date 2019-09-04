20b7ab2c2dc174538f58baf896dfeb24a7c4630d
Delfio <delfio_eu@hotmail.com>
2019-09-03 23:20:04 -0400

    Att

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+
+import { Container, SubmitButton, Form } from './styles';
 
 function Main(){
-  /**
-   * Função dentro de uma propriedade usada no style.js
-   */
-  return <Title>
-    Hello World!
-    </Title>
-}
+  return(
+    <>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
 
+      <Form onSubmit={ ()=>{} }>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+        />
+      <SubmitButton disable>
+        <FaPlus color="#ffff" size={14} />
+      </SubmitButton>
+      </Form>
+    </Container>
+    </>
+  );
+}
 export default Main;
